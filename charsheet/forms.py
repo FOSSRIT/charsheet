9c08817689ad01f87a4e7a9f17cc2cf15d1898a7
David Gay <oddshocks@gmail.com>
2012-07-24 16:28:54 -0400

    home form improvements

--- charsheet/forms.py
+++ charsheet/forms.py
@@ -9,13 +9,17 @@ class CharsheetForm(tw2.forms.FormPage):
         buttons = [tw2.forms.SubmitButton(id='submit', value='Generate')]
         action = '/charsheet'
         id = tw2.forms.HiddenField()
+        master_label = tw2.forms.Label(text='If this field is completed, its \
+                value will be applied to each of the below username fields.')
         master = tw2.forms.TextField('master', label='Master')
         github = tw2.forms.TextField('github', label='GitHub')
         ohloh = tw2.forms.TextField('ohloh', label='Ohloh')
         coderwall = tw2.forms.TextField('coderwall', label='Coderwall')
         stack_exchange = tw2.forms.TextField('stack_exchange',
-            label='Stack Exchange')
+                label='Stack Exchange')
+        fas_label = tw2.forms.Label(text='We cannot authenticate FAS using \
+                OpenID at this time.')
         fedora = tw2.forms.TextField(
             'fedora', label='Fedora Account System (FAS)')
         fedora_pass = tw2.forms.PasswordField(
-            'fedora_pass', label='-\tpassword')
+            'fedora_pass', label='FAS password')
