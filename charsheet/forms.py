c2ad32a3aa31ad90e9f3c92eaed7425948021bbd
David Gay <oddshocks@gmail.com>
2012-07-30 19:52:55 -0400

    tweaks to form, also belated close #32

--- charsheet/forms.py
+++ charsheet/forms.py
@@ -17,13 +17,13 @@ class CharsheetForm(tw2.forms.FormPage):
         coderwall = tw2.forms.TextField('coderwall', label='Coderwall')
 
         spacer1 = tw2.forms.Spacer()
-        se_label = tw2.forms.Label(text='stackexchange.com/users/ID/USERNAME')
         stack_exchange = tw2.forms.TextField('stack_exchange',
                 label='Stack Exchange ID')
+        se_label = tw2.forms.Label(text='stackexchange.com/users/ID/USERNAME')
 
         spacer2 = tw2.forms.Spacer()
-        fas_label = tw2.forms.Label(text='No OpenID at this time. :(')
         fedora = tw2.forms.TextField(
             'fedora', label='Fedora Account System (FAS)')
         fedora_pass = tw2.forms.PasswordField(
             'fedora_pass', label='FAS password')
+        fas_label = tw2.forms.Label(text='No OpenID at this time. :(')
