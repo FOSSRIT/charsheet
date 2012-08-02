6a68cae0b76359f108343aa97cda4d2d8b58858a
David Gay <oddshocks@gmail.com>
2012-08-02 16:02:07 -0400

    more whoops

--- charsheet/forms.py
+++ charsheet/forms.py
@@ -21,7 +21,9 @@ class CharsheetForm(tw2.forms.FormPage):
                 label='Stack Exchange ID')
         se_label = tw2.forms.Label(text='stackexchange.com/users/ID/USERNAME')
 
+        """
         spacer2 = tw2.forms.Spacer()
+        """
         fedora = tw2.forms.HiddenField(
             'fedora', label='Fedora Account System (FAS)')
         fedora_pass = tw2.forms.HiddenField(
