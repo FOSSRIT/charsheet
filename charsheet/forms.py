879ebfb44e14a17c51484ba63bd157c12e5ab8f2
David Gay <oddshocks@gmail.com>
2012-08-02 16:01:13 -0400

    whoops broke it

--- charsheet/forms.py
+++ charsheet/forms.py
@@ -21,11 +21,11 @@ class CharsheetForm(tw2.forms.FormPage):
                 label='Stack Exchange ID')
         se_label = tw2.forms.Label(text='stackexchange.com/users/ID/USERNAME')
 
-        """
         spacer2 = tw2.forms.Spacer()
-        fedora = tw2.forms.TextField(
+        fedora = tw2.forms.HiddenField(
             'fedora', label='Fedora Account System (FAS)')
-        fedora_pass = tw2.forms.PasswordField(
+        fedora_pass = tw2.forms.HiddenField(
             'fedora_pass', label='FAS password')
+        """
         fas_label = tw2.forms.Label(text='No OpenID at this time. :(')
         """
