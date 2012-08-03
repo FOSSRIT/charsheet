fad66ee3ee8588a5ecb7d71fef51a80381325611
David Gay <oddshocks@gmail.com>
2012-08-03 13:37:31 -0400

    add "All fields optional." message to top of form

--- charsheet/forms.py
+++ charsheet/forms.py
@@ -9,22 +9,17 @@ class CharsheetForm(tw2.forms.FormPage):
         buttons = [tw2.forms.SubmitButton(id='submit', value='Generate')]
         action = '/charsheet'
         id = tw2.forms.HiddenField()
+        optional_label = tw2.forms.Label(text="All fields optional.")
         master = tw2.forms.TextField('master', label='Master')
         master_label = tw2.forms.Label(text='Master field autocompletes \
                 these 3:')
         github = tw2.forms.TextField('github', label='GitHub')
         ohloh = tw2.forms.TextField('ohloh', label='Ohloh')
         coderwall = tw2.forms.TextField('coderwall', label='Coderwall')
-
-        spacer1 = tw2.forms.Spacer()
+        fedora = tw2.forms.TextField(
+                'fedora', label='Fedora Account System (FAS)')
+        fedora_pass = tw2.forms.PasswordField(
+                'fedora_pass', label='FAS Password')
         stack_exchange = tw2.forms.TextField('stack_exchange',
                 label='Stack Exchange ID')
         se_label = tw2.forms.Label(text='stackexchange.com/users/ID/USERNAME')
-
-        fedora = tw2.forms.HiddenField(
-                'fedora', label='Fedora Account System (FAS)')
-        fedora_pass = tw2.forms.HiddenField(
-                'fedora_pass', label='FAS password')
-        spacer2 = tw2.forms.Spacer()
-        fas_label = tw2.forms.Label(text='FAS is not available at \
-                this time.')
