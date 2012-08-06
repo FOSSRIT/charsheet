49db46fa9b95f2cbb4d05311e7a54b6b700bf0e2
David Gay <oddshocks@gmail.com>
2012-08-06 17:33:19 -0400

    javascript tweaks

--- charsheet/static/js/charsheet.js
+++ charsheet/static/js/charsheet.js
@@ -1,32 +1,30 @@
 $(document).ready(function() {
     
     // Hide 20 of the 25 events displayed by default
-    $('#recent-activity li').slice(5).hide();
+    $("#recent-activity li").slice(5).hide();
 
     // Handle "more" and "less" buttons
     var showing = 5; // number of items showing
     
-    $('.less-activity').on('click', function() {
+    $(".less-activity").on("click", function() {
         if (showing <= 5) return false;
         
         // Hide 5 more items
-        $('#recent-activity li').slice(showing-5, showing).hide();
+        $("#recent-activity li").slice(showing-5, showing).hide();
         showing = showing - 5;
         return false; // don't move to the top of the page
     });
 
-    $('.more-activity').on('click', function() {
-        console.log('test')
+    $(".more-activity").on("click", function() {
         if (showing >= 25) return false;
-        console.log(showing)
 
         // Show 5 more items
-        $('#recent-activity li').slice(showing, showing+5).show();
+        $("#recent-activity li").slice(showing, showing+5).show();
         showing = showing + 5;
-        console.log(showing)
         return false; // don't move to the top of the page
     });
 
+    // jQuery tooltip plugin stuff
     $('.tooltip').tooltip({
         track: true,
         delay: 250,
