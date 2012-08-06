295c6ed350e50b3c0388c1e9b7bab9b26f95e24c
David Gay <oddshocks@gmail.com>
2012-08-06 17:11:14 -0400

    unittests not yet working, don't enable yet

--- charsheet/tests.py
+++ charsheet/tests.py
@@ -6,29 +6,28 @@ from pyramid import testing
 from .models import DBSession
 
 """
-class TestMyView(unittest.TestCase):
+class TestCharsheet(unittest.TestCase):
     def setUp(self):
         self.config = testing.setUp()
         from sqlalchemy import create_engine
         engine = create_engine('sqlite://')
         from .models import (
             Base,
-            MyModel,
             )
         DBSession.configure(bind=engine)
         Base.metadata.create_all(engine)
         with transaction.manager:
-            model = MyModel(name='one', value=55)
-            DBSession.add(model)
+            # add models here
+            pass
 
     def tearDown(self):
         DBSession.remove()
         testing.tearDown()
 
-    def test_it(self):
-        from .views import my_view
+    def basic_test(self):
+        from .views import charsheet
         request = testing.DummyRequest()
-        info = my_view(request)
+        info = charsheet(request)
         self.assertEqual(info['one'].name, 'one')
         self.assertEqual(info['project'], 'charsheet')
 """
