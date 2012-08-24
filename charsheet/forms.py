138e89060d4e6ccb76838bdd54f6f8e61b49d302
David Gay <oddshocks@gmail.com>
2012-08-24 10:26:04 -0400

    remove SE and FAS from form

--- charsheet/forms.py
+++ charsheet/forms.py
@@ -10,15 +10,6 @@ class CharsheetForm(tw2.forms.FormPage):
         action = '/charsheet'
         id = tw2.forms.HiddenField()
         master = tw2.forms.TextField('master', label='Master')
-        master_label = tw2.forms.Label(text='Master field autocompletes \
-                next three.')
         github = tw2.forms.TextField('github', label='GitHub')
         ohloh = tw2.forms.TextField('ohloh', label='Ohloh')
         coderwall = tw2.forms.TextField('coderwall', label='Coderwall')
-        fedora = tw2.forms.TextField(
-                'fedora', label='Fedora Account System (FAS)')
-        fedora_pass = tw2.forms.PasswordField(
-                'fedora_pass', label='FAS Password')
-        stack_exchange = tw2.forms.TextField('stack_exchange',
-                label='Stack Exchange ID')
-        se_label = tw2.forms.Label(text='stackexchange.com/users/ID/USERNAME')
