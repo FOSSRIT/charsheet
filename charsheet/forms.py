5d4bbad62e8ce7b07b288a2f7a048b76ff067cf5
David Gay <oddshocks@gmail.com>
2012-08-24 22:16:34 -0400

    add master field label (also, things are working)

--- charsheet/forms.py
+++ charsheet/forms.py
@@ -9,6 +9,9 @@ class CharsheetForm(tw2.forms.FormPage):
         buttons = [tw2.forms.SubmitButton(id='submit', value='Generate')]
         action = '/charsheet'
         id = tw2.forms.HiddenField()
+        lbl_master = tw2.forms.Label(text='\
+                <span style="color: red">Master</span> \
+                field autocompletes all fields.')
         master = tw2.forms.TextField('master', label='Master')
         github = tw2.forms.TextField('github', label='GitHub')
         ohloh = tw2.forms.TextField('ohloh', label='Ohloh')
